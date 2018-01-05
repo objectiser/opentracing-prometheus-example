3320e85012332ea7c7a438833625b43604796ce5
Gary Brown <gary@brownuk.com>
2018-01-05 11:12:27 +0000

    Test to see how zipkin copes with tracing from proxies and service instrumentation - seemed to work fine

--- simple/accountmgr/src/main/java/com/example/accountmgr/MetricsConfiguration.java
+++ simple/accountmgr/src/main/java/com/example/accountmgr/MetricsConfiguration.java
@@ -2,14 +2,23 @@ package com.example.accountmgr;
 
 import org.springframework.context.annotation.Bean;
 
-import io.opentracing.contrib.metrics.MetricLabel;
-import io.opentracing.contrib.metrics.label.BaggageMetricLabel;
-import io.opentracing.contrib.metrics.label.ConstMetricLabel;
+import io.opentracing.Tracer;
+//import io.opentracing.contrib.metrics.MetricLabel;
+//import io.opentracing.contrib.metrics.label.BaggageMetricLabel;
+//import io.opentracing.contrib.metrics.label.ConstMetricLabel;
+import zipkin2.codec.SpanBytesEncoder;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.okhttp3.OkHttpSender;
+
 import org.springframework.context.annotation.Configuration;
+
+import brave.Tracing;
+import brave.opentracing.BraveTracer;
 
 @Configuration
 public class MetricsConfiguration {
 
+    /*
     @Bean
     public MetricLabel transactionLabel() {
         return new BaggageMetricLabel("transaction", "n/a");
@@ -19,5 +28,20 @@ public class MetricsConfiguration {
     public MetricLabel versionLabel() {
         return new ConstMetricLabel("version", System.getenv("VERSION"));
     }
+    */
 
+    @Bean
+    public Tracer getTracer() {
+        AsyncReporter reporter = AsyncReporter.create(OkHttpSender.create("http://zipkin.istio-system:9411/api/v2/spans"));
+
+        // Now, create a Brave tracing component with the service name you want to see in Zipkin.
+        //   (the dependency is io.zipkin.brave:brave)
+        Tracing braveTracing = Tracing.newBuilder()
+                              .localServiceName("ACCOUNTMGR")
+                              .spanReporter(reporter)
+                              .build();
+
+        // use this to create an OpenTracing Tracer
+        return BraveTracer.create(braveTracing);
+    }
 }
