d73ec52137417ea5d40e85508b2695bdadd77ea1
Gary Brown <gary@brownuk.com>
2018-01-15 10:37:02 +0000

    Order mgr with manual context propagation in 'buy' operation

--- simple/ordermgr/src/main/java/com/example/ordermgr/OrderMgrController.java
+++ simple/ordermgr/src/main/java/com/example/ordermgr/OrderMgrController.java
@@ -1,14 +1,15 @@
 package com.example.ordermgr;
 
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import io.opentracing.ActiveSpan;
 
 @RestController
 public class OrderMgrController {
@@ -19,24 +20,30 @@ public class OrderMgrController {
     private static String accountMgrUrl = System.getProperty("ACCOUNTMGR_URL", System.getenv("ACCOUNTMGR_URL"));
 
     @Autowired
-    private io.opentracing.Tracer tracer;
+    private HttpServletRequest request;
 
     @RequestMapping("/buy")
     public String buy() throws InterruptedException {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("myname","jasmi");
+        requestHeaders.set("user-agent", request.getHeader("user-agent"));
+        requestHeaders.set("x-request-id", request.getHeader("x-request-id"));
+        requestHeaders.set("x-b3-traceid", request.getHeader("x-b3-traceid"));
+        requestHeaders.set("x-b3-spanid", request.getHeader("x-b3-spanid"));
+        requestHeaders.set("x-b3-parentspanid", request.getHeader("x-b3-parentspanid"));
+        requestHeaders.set("x-b3-sampled", request.getHeader("x-b3-sampled"));
+        requestHeaders.set("x-b3-flags", request.getHeader("x-b3-flags"));
+        requestHeaders.set("x-ot-span-context", request.getHeader("x-ot-span-context"));
+        
+        HttpEntity<String> entity = new HttpEntity<String>(requestHeaders);
         Thread.sleep(1 + (long)(Math.random()*500));
-        Optional.ofNullable(tracer.activeSpan()).ifPresent(as -> as.setBaggageItem("transaction", "buy"));
-        try (ActiveSpan span = tracer.buildSpan("SomeWork").startActive()) {
-            span.setTag("work", "buying");
-
-            ResponseEntity<String> response = restTemplate.getForEntity(accountMgrUrl + "/account", String.class);
-            return "BUY + " + response.getBody();
-        }
+        ResponseEntity<String> response = restTemplate.exchange(accountMgrUrl + "/account", HttpMethod.GET, entity, String.class);
+        return "BUY + " + response.getBody();
     }
 
     @RequestMapping("/sell")
     public String sell() throws InterruptedException {
         Thread.sleep(1 + (long)(Math.random()*500));
-        Optional.ofNullable(tracer.activeSpan()).ifPresent(as -> as.setBaggageItem("transaction", "sell"));
         ResponseEntity<String> response = restTemplate.getForEntity(accountMgrUrl + "/account", String.class);
         return "SELL + " + response.getBody();
     }
